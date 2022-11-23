23e65f84a33f15a2131e1acf8f243e47d4cbbe75
hikio-17 <fajri010217@gmail.com>
2022-11-22 19:05:11 -0800

    create get all products controller

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -9,9 +9,12 @@ exports.newProduct = async (req, res, next) => {
   });
 };
 
-exports.getProducts = (req, res, next) => {
-  res.json({
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
     success: true,
-    message: "This route will show all products in database",
+    count: products.length,
+    products,
   });
 };
