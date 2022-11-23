a4024c4bbfff58d129ac8f81f7fd49c7e116ed7d
hikio-17 <fajri010217@gmail.com>
2022-11-22 19:21:43 -0800

    create getSingleProduct Controller

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -18,3 +18,19 @@ exports.getProducts = async (req, res, next) => {
     products,
   });
 };
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id).exec();
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,9 +4,12 @@ const router = require("express").Router();
 const {
   getProducts,
   newProduct,
+  getSingleProduct,
 } = require("../controllers/productsController");
 
 router.get("/products", getProducts);
+router.get("/product/:id", getSingleProduct);
+
 router.post("/product/new", newProduct);
 
 module.exports = router;
