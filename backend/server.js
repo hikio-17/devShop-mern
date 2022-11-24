84223f40842b6e28b3fd4f62a7f8d671b0797720
hikio-17 <fajri010217@gmail.com>
2022-11-24 07:24:47 -0800

    create handle unhandled Promise Rejections & Uncaught Exceptions

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,33 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
+// handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR ==> ${err.message}`);
+  console.log("Shutting down the server due to uncaught exceptions");
+  process.exit(1);
+});
+
 // set up konfigurasi env
 dotenv.config({
   path: "backend/config/config.env",
 });
+console.log(a);
 
 // connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server running at http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+// Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error ==> ${err.message}`);
+  console.log(`Shutting down the server due to unhandled Promise rejections`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
