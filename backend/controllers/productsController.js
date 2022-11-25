465327d192056668f9ff27e44dbba06e9920eccc
hikio-17 <fajri010217@gmail.com>
2022-11-24 17:19:33 -0800

    create api features ==> search(keyword)

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/cathAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
 
 // create product ==> /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,7 +15,9 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // get all products ==> /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
