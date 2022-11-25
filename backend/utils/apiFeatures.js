1f7a08598e9d07a1d2da860347ad77dd57881870
hikio-17 <fajri010217@gmail.com>
2022-11-24 19:00:15 -0800

    create API Features ==> filter by category & price

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -14,8 +14,26 @@ class APIFeatures {
         }
       : {};
 
-    console.log(keyword);
+    //  console.log(keyword);
     this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    //Removing fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    console.log(queryCopy);
+    // filter for price, rating etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    console.log(queryStr);
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 }
