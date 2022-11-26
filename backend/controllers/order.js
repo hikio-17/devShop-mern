5884acabf62ceeeeaa97b5cd42ca0470e9262e36
hikio-17 <fajri010217@gmail.com>
2022-11-26 06:13:13 -0800

    Create controller  update order status --> admin

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,8 +1,8 @@
 const Order = require("../models/order");
+const Product = require("../models/product");
 
 const cathAsyncErrors = require("../middlewares/cathAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
-const order = require("../models/order");
 
 exports.newOrder = cathAsyncErrors(async (req, res, next) => {
   const {
@@ -76,3 +76,35 @@ exports.allOrders = cathAsyncErrors(async (req, res, next) => {
     orders,
   });
 });
+
+// Update Order --admin ==> /api/v1/order/:id
+exports.updateOrder = cathAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "Delivered") {
+    return next(
+      new ErrorHandler("You have already delivered this product", 400)
+    );
+  }
+  // update stock product
+  order.orderItems.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -6,17 +6,25 @@ const {
   getSingleOrder,
   myOrders,
   allOrders,
+  updateOrder,
 } = require("../controllers/order");
 const { isAuthenticatedUser, authorizedRoles } = require("../middlewares/auth");
 
 router.post("/order/new", isAuthenticatedUser, newOrder);
 router.get("/order/:id", isAuthenticatedUser, getSingleOrder);
 router.get("/orders/me", isAuthenticatedUser, myOrders);
+
 router.get(
   "/admin/orders",
   isAuthenticatedUser,
   authorizedRoles("admin"),
   allOrders
+);
+router.put(
+  "/admin/order/:id",
+  isAuthenticatedUser,
+  authorizedRoles("admin"),
+  updateOrder
 );
 
 module.exports = router;
