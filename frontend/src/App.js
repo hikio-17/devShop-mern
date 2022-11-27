5847f0b9c4077fcd3c2d454b23c023ad507312d7
hikio-17 <fajri010217@gmail.com>
2022-11-27 07:28:27 -0800

    create product details (FE)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import "./App.css";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/product/:id" exact element={<ProductDetails />} />
       </Routes>
       <Footer />
     </>
--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   ALL_PRODUCTS_REQUEST,
   ALL_PRODUCTS_SUCCESS,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "./../constants/productConstants";
 import axios from "axios";
 
@@ -21,6 +24,25 @@ export const getProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(
+      `http://localhost:5000/api/v1/product/${id}`
+    );
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response.data.message,
     });
   }
